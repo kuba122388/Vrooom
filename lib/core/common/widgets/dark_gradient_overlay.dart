ab40ed08d3ca40dfb8d9bb4dc56576f7036115a7
Jakub <245762@edu.p.lodz.pl>
2025-10-15 12:39:28 +0200

    feat: add reusable card and overlay components
    - Create DarkGradientOverlay for image overlays
    - Create InfoSectionCard for structured content sections

---
+++ lib/core/common/widgets/dark_gradient_overlay.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DarkGradientOverlay extends StatelessWidget {
+  final double height;
+
+  const DarkGradientOverlay({super.key, required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          colors: [
+            Colors.black.withOpacity(0.5),
+            Colors.black.withOpacity(0.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
