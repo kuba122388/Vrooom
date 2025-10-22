29ce5d86e7a7721dfcff46985d3eaa48ad14a2fc
Jakub <115095337+kuba122388@users.noreply.github.com>
2025-10-22 11:58:03 +0200

    Refactor: Restructure admin directory from user to presentation

---
+++ lib/presentation/admin/car_management/pages/car_management_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CarManagementPage extends StatelessWidget {
+  const CarManagementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
