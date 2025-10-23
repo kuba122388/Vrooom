ececc1dd5a7e2a5f1feea15941e7e603fb70d7d4
SzkopikRafal <117367103+SzkopikRafal@users.noreply.github.com>
2025-10-23 18:54:35 +0200

    Feature: Add future reservations page (#18)

---
+++ lib/presentation/admin/future_reservation/pages/future_reservation_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:vrooom/core/common/widgets/custom_app_bar.dart';
+import 'package:vrooom/core/common/widgets/search_filter_module.dart';
+import 'package:vrooom/presentation/admin/widgets/rental_information_entry.dart';
+
+import '../../../../core/configs/assets/app_images.dart';
+import '../../../../core/configs/theme/app_spacing.dart';
+
+class FutureReservation extends StatelessWidget {
+  const FutureReservation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<RentalInformationEntry> rentalInformation = [
+      RentalInformationEntry(
+        profileImage: AppImages.person,
+        firstName: "Marek",
+        surname: "Mostowiak",
+        email: "marek.mostowiak@example.com",
+        reservationID: "RENT001",
+        pickupDate: DateTime.now(),
+        returnDate: DateTime.now().add(const Duration(days: 5)),
+        rentalStatus: RentalStatus.pending,
+        carImage: AppImages.mercedes,
+        model: "Mercedes-Benz",
+        productionYear: 2022,
+      ),
+
+      RentalInformationEntry(
+        profileImage: AppImages.person,
+        firstName: "Kacper",
+        surname: "Klimkiewicz",
+        email: "k.klimkiewicz@test.com",
+        reservationID: "RENT002",
+        pickupDate: DateTime.now(),
+        returnDate: DateTime.now().add(const Duration(days: 5)),
+        rentalStatus: RentalStatus.confirmed,
+        carImage: AppImages.mercedes,
+        model: "Mercedes-Benz",
+        productionYear: 2021,
+      ),
+
+      RentalInformationEntry(
+        profileImage: AppImages.person,
+        firstName: "Henryk",
+        surname: "Sienkiewicz",
+        email: "hs@example.com",
+        reservationID: "RENT003",
+        pickupDate: DateTime.now(),
+        returnDate: DateTime.now().add(const Duration(days: 5)),
+        rentalStatus: RentalStatus.cancelled,
+        carImage: AppImages.mercedes,
+        model: "Mercedes-Benz",
+        productionYear: 2022,
+      )
+    ];
+
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: "Future Reservations"
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SearchFilterModule(),
+              const SizedBox(height: AppSpacing.sm),
+              const Text(
+                "Upcoming Rentals",
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: -0.5,
+                ),
+              ),
+              const SizedBox(height: AppSpacing.xs),
+              ...rentalInformation.map((entry) {
+                return RentalInformationEntry(
+                  profileImage: entry.profileImage,
+                  firstName: entry.firstName,
+                  surname: entry.surname,
+                  email: entry.email,
+                  reservationID: entry.reservationID,
+                  pickupDate: entry.pickupDate,
+                  returnDate: entry.returnDate,
+                  rentalStatus: entry.rentalStatus,
+                  carImage: entry.carImage,
+                  model: entry.model,
+                  productionYear: entry.productionYear,
+                );
+              }).expand(
+                  (widget) => [widget, const SizedBox(height: AppSpacing.sm)]
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
